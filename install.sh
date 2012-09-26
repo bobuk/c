a1f309447cfbf0d837a37b704f4d33ce6605380a
Grigory Bakunov <thebobuk@ya.ru>
2012-09-27 01:13:03 +0400

    first install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+mkdir -p ~/bin/
+cp -p c ~/bin/
+pushd . >/dev/null
+cd ~/bin/
+if [ `uname` == 'Darwin' ]; then
+CTRL="cmd"
+else
+CTRL="ctrl"
+fi
+for x in '' '+c' '+v' '+R' '+del' '+h'; do
+    ln -s c $CTRL$x
+done
+popd >/dev/null
